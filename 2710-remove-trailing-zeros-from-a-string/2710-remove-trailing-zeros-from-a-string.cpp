15a0d51c2918c59447e1b2782b540afc6422e17a
Abinav GT <153423887+abinavgt@users.noreply.github.com>
2025-12-08 23:24:15 +0530

    Time: 0 ms (100%), Space: 12 MB (15.63%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
@@ -1,18 +1,17 @@
-
-
 class Solution {
 public:
-    string removeTrailingZeros(string num) {
-        int n = num.size();
-        string ans = "";
+    string removeTrailingZeros(string nums) {
+        
+        int n = nums.size();
+        int i = n-1;
 
-        int i = n - 1;
-        while(i >= 0 && num[i] == '0') i--;  
-
-        for(int j = 0; j <= i; j++){
-            ans.push_back(num[j]);
+        while(i>=0 && nums[i]=='0'){
+            i--;
         }
-
-        return ans;
+        string output = ""; //creates an empty string ,where the final answer is stored
+        for(int j=0;j<=i;j++){
+            output.push_back(nums[j]);
+        }
+        return output;
     }
 };
