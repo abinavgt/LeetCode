302ef391ada5eddc01457e225216e0670d90d1c4
Kyro <153423887+abinavgt@users.noreply.github.com>
2025-12-18 23:50:58 +0530

    Time: 7 ms (24.64%), Space: 13.7 MB (18.47%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -2,36 +2,39 @@ class Solution {
 public:
     int calculate(string s) {
         
-        int n = s.size();
+        //Basic Calculator Theory 
         long long int sum =0;
-        int sign = 1; //this can be +1 or -1 either ,depends on the opeartion
-        stack<pair<int,int>>st;
+        int sign = 1; // This sign detrmine whether +1 or -1 depends on operation
+        stack<pair<int,int>> st; //Stores the sum and sign;
 
-        for(int i=0;i<n;i++){
-            if(isdigit(s[i])){ //if the string is integer from '0 to 9'
-                long long int num =0;
+        for(int i=0;i<s.size();i++){
+            //Initially we are checking string belong to integer or not '0 to 9'
+            long long int num =0;
+            if(isdigit(s[i])){
                 while(s.size()>0 && isdigit(s[i])){
-
-                    num = num * 10 +(s[i] - '0'); //here the 's[i]-0' this converts string to integer 
+                    num = num * 10 + ((s[i])-'0'); //This coverts string to integer
                     i++;
                 }
-                i--;
+                //After checking , if it is integer then it performs the calculation
+                i--; //again starts from index 0
                 sum += num*sign;
-                sign =1;
+                sign =1; //again reset sign to 1
             }
             else if(s[i]=='-'){
                 sign = -1*sign;
             }
-            else if(s[i] =='('){
+            else if(s[i]=='('){
                 st.push({sum,sign});
-                sum =0;
+                sum =0; //Reset values to the default, checks the cal inside the brackets
                 sign =1;
             }
             else if(s[i]==')'){
-                sum = st.top().first + (st.top().second * sum);
+                sum = st.top().first + st.top().second * sum;
                 st.pop();
+                // Here the st.top().first() is value stored in stack and second is ootside
             }
         }
         return sum;
+
     }
 };
